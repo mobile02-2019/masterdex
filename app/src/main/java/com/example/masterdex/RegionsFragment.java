1a3451f9628d662edf4d3606a6002febbb337c39
Eduardo <eduardo_mizael@outlook.com.br>
2019-06-28 14:27:11 -0300

    Layout pronto com RecyclerView, adicionar onCLick

--- app/src/main/java/com/example/masterdex/RegionsFragment.java
+++ app/src/main/java/com/example/masterdex/RegionsFragment.java
@@ -1,17 +1,28 @@
 package com.example.masterdex;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.masterdex.adapter.RegioesAdapter;
+import com.example.masterdex.models.Regiao;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RegionsFragment extends Fragment {
+
+    private RecyclerView recyclerView;
 
 
     public RegionsFragment() {
@@ -24,10 +35,23 @@ public class RegionsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_regions, container, false);
 
+        recyclerView = view.findViewById(R.id.regioes_recycler_view);
 
-        // trabalhe aqui dentro
+        RegioesAdapter regioesAdapter = new RegioesAdapter(getListaRegioes());
 
+        RecyclerView.LayoutManager layoutManager =new GridLayoutManager(getContext(), 3);
+
+        recyclerView.setAdapter(regioesAdapter);
+        recyclerView.setLayoutManager(layoutManager);
         return view;
     }
 
+    private List<Regiao> getListaRegioes(){
+        List<Regiao> listaRegioes = new ArrayList<>();
+
+        Regiao kanto = new Regiao("Kanto");
+        listaRegioes.add(kanto);
+
+        return  listaRegioes;
+    }
 }
---
+++ app/src/main/java/com/example/masterdex/models/Regiao.java
@@ -0,0 +1,18 @@
+package com.example.masterdex.models;
+
+public class Regiao {
+
+    private String nomeRegiao;
+
+    public Regiao(String nomeRegiao) {
+        this.nomeRegiao = nomeRegiao;
+    }
+
+    public String getNomeRegiao() {
+        return nomeRegiao;
+    }
+
+    public void setNomeRegiao(String nomeRegiao) {
+        this.nomeRegiao = nomeRegiao;
+    }
+}
