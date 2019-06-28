667a351f3d85a8186943a94c3ac25eecd9b690b1
michael <michaelpinheiro00@gmail.com>
2019-06-28 10:24:29 -0300

    botão perfil ok

--- app/src/main/java/com/example/masterdex/Perfil.java
+++ app/src/main/java/com/example/masterdex/Perfil.java
@@ -3,19 +3,38 @@ package com.example.masterdex;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
 
 public class Perfil extends AppCompatActivity {
+
+    Button buttonOpcoesPerfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        buttonOpcoesPerfil = findViewById(R.id.button_opcoes_perfil);
+
+        buttonOpcoesPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                PopupMenu popupMenu = new PopupMenu(Perfil.this, buttonOpcoesPerfil);
+                MenuInflater menuInflater = popupMenu.getMenuInflater();
+                menuInflater.inflate(R.menu.menu_opcoes_perfil, popupMenu.getMenu());
+                popupMenu.show();
+
+            }
+        });
+
+
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.testeopcoes, menu);
-        return true;
-    }
+
 }
